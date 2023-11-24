0852781115c924c2883aed654e7fb3b412c3e5a5
Jess Espinoza <Jespinozalegri24@gmail.com>
2023-11-24 08:41:07 -0600

    cambio en buscarTerminales.js

--- public/js/buscarTerminales.js
+++ public/js/buscarTerminales.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const formulario = document.getElementById('buscar-terminal-form');
-    const resultadosDiv = document.getElementById('resultados-terminales');
+document.getElementById('buscar-terminal-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const ciudad = document.getElementById('ciudad').value.toLowerCase();
 
-    formulario.addEventListener('submit', function(e) {
-        e.preventDefault();
-        const ciudad = document.getElementById('ciudad').value;
+    fetch('/buscar-terminales', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ciudad: ciudad })
+    })
+    .then(response => response.json())
+    .then(terminales => {
+        const resultadosDiv = document.getElementById('resultados-terminales');
+        resultadosDiv.innerHTML = ''; // Limpiar resultados anteriores
 
-        fetch('/buscar-terminales', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ ciudad: ciudad })
-        })
-        .then(response => response.json())
-        .then(terminales => {
-            resultadosDiv.innerHTML = ''; // Limpiar resultados anteriores
+        if (terminales.length > 0) {
             terminales.forEach(terminal => {
-                // Asegúrate de que el campo 'nombre' exista en tu esquema de base de datos
-                resultadosDiv.innerHTML += `<p>${terminal.nombre}</p>`;
+                resultadosDiv.innerHTML += `<p>${terminal.terminal}</p>`;
             });
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            resultadosDiv.innerHTML = '<p>Hubo un error al buscar las terminales.</p>';
-        });
+        } else {
+            resultadosDiv.innerHTML = '<p>No se encontraron terminales para esta ciudad.</p>';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        resultadosDiv.innerHTML = '<p>Hubo un error al buscar las terminales.</p>';
     });
 });
